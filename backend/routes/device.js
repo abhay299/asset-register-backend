f5ba1c949f78b2bc8fe63814003e984663850c67
abhay299 <abhaysg2000@gmial.com>
2023-11-23 17:31:01 +0200

    commit

--- backend/routes/device.js
+++ backend/routes/device.js
@@ -1,18 +1,6 @@
 const router = require("express").Router();
 const Data = require("../models/Data.js");
 
-// Create Device
-router.post("/", async (req, res) => {
-  const newDevice = new Data(req.body);
-
-  try {
-    const savedDevice = await newDevice.save();
-    res.status(200).json(savedDevice);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get("/", async (req, res) => {
   try {
     // Retrieve data from the collection
@@ -23,32 +11,5 @@ router.get("/", async (req, res) => {
     res.status(500).send(`Error: ${error.message}`);
   }
 });
-
-// Get All Devices
-// router.get("/", async (req, res) => {
-//   const queryNew = req.query.new;
-//   const queryCategory = req.query.category;
-
-//   try {
-//     let devices;
-
-//     if (queryNew) {
-//       devices = await Device.find();
-//       //   devices = await Device.find().sort({ createdAt: -1 }).limit(5);
-//     } else if (queryCategory) {
-//       devices = await Device.find({
-//         categories: {
-//           $in: [queryCategory],
-//         },
-//       });
-//     } else {
-//       devices = await Device.find();
-//     }
-//     console.log("Devices: ", devices);
-//     res.status(200).json(devices);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
